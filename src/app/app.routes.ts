efd0766bd08eff4f66d06ceeebdd045425498723
abdallahAhmed <88485785+abdalla5555887@users.noreply.github.com>
2025-08-04 02:29:06 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DetailsComponent } from './pages/details/details.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { authGuard } from './core/guards/auth/auth.guard';
 import { log } from 'console';
